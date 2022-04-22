eefb9a584175940c1c93b777b5fe6901835aed65
Екатерина Жериборова <artprojectt@mail.ru>
2022-04-22 20:42:46 +0500

    Увеличен размер модели

--- main_mask.py
+++ main_mask.py
@@ -3,13 +3,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
 
-# unmasker = pipeline("fill-mask", model="albert-base-v2")
-#
-# print(unmasker("I am a [MASK].")[0]['sequence'])
-#
-#
-# class Item(BaseModel):
-#     text: str
+unmasker = pipeline("fill-mask", model="albert-base-v2")
+
+print(unmasker("I am a [MASK].")[0]['sequence'])
+
+
+class Item(BaseModel):
+    text: str
 
 
 app = FastAPI()
@@ -19,11 +19,10 @@ app = FastAPI()
 def root():
     return {"message": "All right, there is a connection."}
 
-
-# @app.post("/predict/")
-# def predict(item: Item):
-#     """Предварительно подготовленная модель на английском языке.
-#     Вместо [MASK] моделирует(подбирает) слово.
+@app.post("/predict/")
+def predict(item: Item):
+    """Предварительно подготовленная модель на английском языке.
+    Вместо [MASK] моделирует(подбирает) слово.
 #     Источник - https://huggingface.co/albert-base-
 #     Примеры фраз:  I study economics at University., I like apples and  pears., My friend often travels.,
 #     I always get up at 8 o’clock in the morning., We have a flat in London., He plays football every Saturday.,
