e4a987bc5ecc918cbd39f23a6edbdaac95a16ff5
Ekaterina <80875367+EkaterinaVZ@users.noreply.github.com>
2022-05-31 11:19:12 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,11 +9,4 @@ echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
-[theme]\n\
-# светлая тема\n\
-primaryColor='#6eb52f'\n\
-backgroundColor="#f0f0f5" \\n\
-secondaryBackgroundColor="#e0e0ef"\n\
-textColor="#262730"\\n\
-font="sans serif"\\n\
 " > ~/.streamlit/config.toml
