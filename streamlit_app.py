e82c518eed153b85dacb188b773645ab93bb3f61
Ekaterina <80875367+EkaterinaVZ@users.noreply.github.com>
2022-06-20 21:42:38 +0500

    Update streamlit_app.py
    Внесены исправления в код. Устранены все замечания.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,25 +2,26 @@
 # Our streamlit app
 """
 
+import time
 
+import streamlit as st
+from transformers import pipeline
 
 
 def load_model():
     model = pipeline("fill-mask", model="albert-base-v2")
     return model
 
-import time, streamlit as st
-from transformers import pipeline
 
 model = load_model()
 
 #  логотип и название
-a, b = st.columns([1, 1])
+col1, col2 = st.columns([1, 1])
 
-with a:
+with col1:
     st.header('Model "Fill mask"')
 
-with b:
+with col2:
     st.image("books.png")
 
 # Боковая панель
